446a0dac527a69cdbfdb3ceda1b0791aecca46db
EmmanuelM7 <mormani11@gmail.com>
2023-03-12 08:58:28 -0400

    Did Counter Assignment

--- src/Counter.js
+++ src/Counter.js
@@ -4,9 +4,16 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   function increment() {
     // Fill in code for increment here
+    var countInc = count;
+    countInc += 1;
+    setCount(countInc);
   }
   function decrement() {
     // Fill in code for decrement here
+    var countDec = count;
+    countDec -= 1;
+    setCount(countDec);
+
   }
   return (
     <div>
